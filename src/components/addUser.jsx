ad879f656907fb628e6c67e2c21fb9c2036f033c
NwachukwuSam <nwachukwusamuel123@gmail.com>
2023-10-06 16:20:09 +0100

    add user

--- src/components/addUser.jsx
+++ src/components/addUser.jsx
@@ -1,9 +1,24 @@
 import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser } from '../features/userReducer';
+import { useNavigate } from 'react-router-dom';
+
 
 function AddUser() {
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
+    const [age, setAge] = useState('');
+    const [gender, setGender] = useState('');
 
+    const users = useSelector((state) => state.users);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        dispatch(addUser({id: users[users.length-1].id +1, name, email, age, gender}))
+        navigate("/successful");
+    }
   return (
 
     <div className=' justify-center'>
@@ -18,7 +33,17 @@ function AddUser() {
                 <input type="text" id="email" placeholder="enter your email example@gmail.com" className='mt-[10px] ml-[10px] px-[180px] py-2 rounded-md'
                 onChange={e=> setEmail(e.target.value)}/>
             </div>
-            <button className="bg-green-500 rounded-md text-white py-2 px-4 mt-[20px] ml-[290px] hover:bg-blue-500">Submit</button>
+            <div className=' ml-[10px]'>
+            <label htmlFor="age" className='text-white'>Age:</label>
+                <input type="number" id="email" placeholder="enter your Age" className='mt-[10px] ml-[10px] px-[180px] py-2 rounded-md'
+                onChange={e=> setAge(e.target.value)}/>
+            </div>
+            <div className=' ml-[10px]'>
+            <label htmlFor="gender" className='text-white'>Gender:</label>
+                <input type="text" id="email" placeholder="enter your gender" className='mt-[10px] ml-[10px] px-[180px] py-2 rounded-md'
+                onChange={e=> setGender(e.target.value)}/>
+            </div>
+            <button  onClick= {handleSubmit} className="bg-green-500 rounded-md text-white py-2 px-4 mt-[20px] ml-[290px] hover:bg-blue-500">Submit</button>
         </div>
 
     </div>
